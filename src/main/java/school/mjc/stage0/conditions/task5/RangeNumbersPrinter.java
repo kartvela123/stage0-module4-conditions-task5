e6d55502580fc0ce2151d1bebf357fe8b4c33a8d
kart <mkartveli9@gmail.com>
2023-02-18 21:18:26 +0000

    4.5

--- src/main/java/school/mjc/stage0/conditions/task5/RangeNumbersPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/RangeNumbersPrinter.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task5;
 
 public class RangeNumbersPrinter {
     public void printRange(int number) {
+        if (number <= 5 && number >= 1) {
+            System.out.println("number is between 1 and 5");
+        } else if (number <= 10 && number >= 6) {
+            System.out.println("number is between 6 and 10");
+        } else {
+            System.out.println("out of range");
+        }
     }
 }
